bf3748bc08975a82f14df1c81fd2d948f762a232
Jonathan A Rees <jar398@mumble.net>
2017-11-04 16:09:23 -0400

    sharing between si/eol/get_images and si/eol/images

--- tests/test_si_eol_get_images.py
+++ tests/test_si_eol_get_images.py
@@ -10,10 +10,7 @@ import webapp
 url = 'http://phylo.cs.nmsu.edu:5004/phylotastic_ws/si/eol/get_images'
 service = webapp.get_service(url)
 
-class TestSiEolGetImages(webapp.WebappTestCase):
-    @classmethod
-    def get_service(self):
-        return service
+class SiEolImagesTester(webapp.WebappTestCase):
 
     def test_bad_method(self):
         request = service.get_request('POST', {})
@@ -22,17 +19,81 @@ class TestSiEolGetImages(webapp.WebappTestCase):
         self.assertEqual(x.status_code, 405)
         # TBD: check for informativeness
 
+    def test_no_parameter(self):
+        request = service.get_request('POST', {})
+        x = self.start_request_tests(request)
+        self.assertTrue(x.status_code == 400)
+        m = x.json().get(u'message')
+        self.assertTrue(u'species' in m,    #informative?
+                        'no "species" in "%s"' % m)
+
+    # What if the supplied parameter name is wrong?  Similar to previous
+
+    def test_bad_parameter(self):
+        request = service.get_request('POST', {u'bad_parameter': []})
+        x = self.start_request_tests(request)
+        self.assertTrue(x.status_code == 400)
+        m = x.json().get(u'message')
+        self.assertTrue(u'species' in m,    #informative?
+                        'no "species" in "%s"' % m)
+
+    # What if the value is a single species name instead of a list?
+    # 18 seconds (!) - doc says expected response time 2s - 6s.
+    # 76 metadata blobs are returned.  TBD: issue.
+
+    def test_bad_value_type(self):
+        print 'Patience, this may take 20 seconds'
+        request = service.get_request('POST', {u'species': u'Nosuchtaxonia mistakea'})
+        x = self.start_request_tests(request)
+        self.assertTrue(x.status_code % 100 == 4, x.status_code)
+        json.dump(x.to_dict(), sys.stdout, indent=2)
+        # TBD: Change this to a *correct* check for message informativeness.
+        m = x.json().get(u'message')
+        self.assertTrue(u'species' in m,    #informative?
+                        'no "species" in "%s"' % m)
+
+    def test_bad_name(self):
+        request = service.get_request('POST', {u'species': [u'Nosuchtaxonia mistakea']})
+        x = self.start_request_tests(request)
+        m = x.json().get(u'message')
+        self.assert_success(x, m)
+        # gives a 200, which is acceptable
+        self.assertTrue(u'species' in x.json())
+        # json.dump(x.to_dict(), sys.stdout, indent=2)
+        self.assertEqual(len(all_images(x)), 0, "number of images")
+
+    # Too many species?
+
     # Insert here: edge case tests
     # Insert here: inputs out of range, leading to error or long delay
     # Insert here: error-generating conditions
     # (See ../README.md)
+
+class TestSiEolGetImages(SiEolImagesTester):
+    @classmethod
+    def get_service(self):
+        return service
+
+    @classmethod
+    def http_method(self):
+        return 'GET'
+
+    # What if you do a GET when the service is expecting a POST?
+    def test_bad_method(self):
+        request = service.get_request('GET', {})
+        x = self.start_request_tests(request)
+        # GET method not allowed
+        self.assertEqual(x.status_code, 405)
+        # TBD: check for informativeness
+        json.dump(x.to_dict(), sys.stdout, indent=2)
 
     def test_example_19(self):
         x = self.start_request_tests(example_19)
         self.assert_success(x)
         # Insert: whether result is what it should be according to docs
 
-null=None; false=False; true=True
+def all_images(x):
+    return [image for source in x.json()[u'species'] for image in source[u'images']]
 
 example_19 = service.get_request('GET', {u'species': u'Panthera leo|Panthera onca|Panthera pardus'})
 
